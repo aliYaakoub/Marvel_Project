aed0dcd56a32b2a7e0672e1968ffe59ecf1bbe7e
ali yaakoub <ali.yaakoub.2021@gmail.com>
2021-11-12 23:19:57 +0200

    changing the tailwind configuration .

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-const tailwindcss = require('tailwindcss');
-
-module.exports = {
-    plugins: [
-        tailwindcss('./tailwind.js'),
-        // tailwindcss('./tailwind.config.js'),
-        require('autoprefixer')
-    ]
-}
